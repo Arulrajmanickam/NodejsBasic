90324ed49bfda4214ff68dabe174d03c29bc1ed0
Arulrajmanickam <arulrajm2212@gmail.com>
2022-01-29 21:09:21 +0530

    added process env port

--- calculator.js
+++ calculator.js
@@ -3,21 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res)=>{
-    //res.send("Hello world");
-    res.sendFile(__dirname + "/index.html");
-})
+app.get("/", (req, res) => {
+  //res.send("Hello world");
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post('/', (req, res)=>{
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send("The result is: " + result);
+});
 
-    let num1 = Number(req.body.num1);
-    let num2 = Number(req.body.num2);
-    let result = num1 + num2;
-    res.send("The result is: " + result );
-})
+const port = process.env.port || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Port is running on 3000")
-})
+app.listen(port, () => {
+  console.log("Port is running on 3000");
+});
